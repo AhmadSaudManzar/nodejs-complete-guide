d22a68d617f8fbc57092e13cd84b70cf5ad184b2
Ahmad Saud Manzar <ahmad.manzar@queensu.ca>
2020-05-28 09:09:43 -0400

    ejs partials refactor

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Find the global module, which ships with node.js 
 const http = require('http');
 //const routes = require('./routes');
-const express = require('express'); 
-const bodyParser = require('body-parser'); 
+const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 
-const app = express(); 
+const app = express();
 
 // Setting templating engine
 app.set('view engine', 'ejs');
@@ -16,17 +16,20 @@ const adminRoutes = require('./routes/admin');
 
 const shopRoutes = require('./routes/shop')
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes.routes); 
-app.use(shopRoutes); 
+app.use('/admin', adminRoutes.routes);
+app.use(shopRoutes);
 
 // 404 Error Route, when no matches
 app.use((req, res, next) => {
     //res.status(404).send('<h1>Page Not Found</h1>')
     //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', {pageTitle : 'Page Not Found'}); 
+    res.status(404).render('404', {
+        pageTitle: 'Page Not Found',
+        path: '/pageNotFound'
+    });
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,15 @@
-const express = require('express'); 
+const express = require('express');
 
-const path = require('path'); 
+const path = require('path');
 
 const rootDir = require('../util/path')
 
-const router = express.Router(); 
+const router = express.Router();
 
 const products = [];
 
-router.get('/add-product', (req, res, next) =>{
-    console.log('In the next Middleware'); 
+router.get('/add-product', (req, res, next) => {
+    console.log('In the next Middleware');
     //res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
     //res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
     //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
@@ -24,7 +24,7 @@ router.get('/add-product', (req, res, next) =>{
 
 router.post('/add-product', (req, res, next) => {
     //console.log(req.body)
-    products.push({title: req.body.title});
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
