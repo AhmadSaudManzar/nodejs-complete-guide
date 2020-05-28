c21a44100e1f9cb4f092345bc3636bd8ebc55ee3
Ahmad Saud Manzar <ahmad.manzar@queensu.ca>
2020-05-28 06:00:08 -0400

    working_with_expressJS

---
+++ app.js
@@ -0,0 +1,32 @@
+// Find the global module, which ships with node.js 
+const http = require('http');
+//const routes = require('./routes');
+const express = require('express'); 
+const bodyParser = require('body-parser'); 
+const path = require('path');
+
+const app = express(); 
+
+// Setting templating engine
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+// Router Object 
+const adminRoutes = require('./routes/admin');
+
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+app.use('/admin',adminRoutes); 
+app.use(shopRoutes); 
+
+// 404 Error Route, when no matches
+app.use((req, res, next) => {
+    //res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
+
---
+++ routes/shop.js
@@ -0,0 +1,16 @@
+const express = require('express'); 
+
+const path = require('path'); 
+
+const rootDir = require('../util/path')
+
+const router = express.Router(); 
+
+router.get('/', (req, res, next) =>{
+    //console.log('In the next Middleware'); 
+    //res.send('<h1>Hello From Express</h1>')
+    //res.sendFile(path.join(__dirname,'../', 'views', 'shop.html')); 
+    res.sendFile(path.join(rootDir, 'views', 'shop.html')); 
+});
+
+module.exports = router; 
