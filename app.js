cc80d228c6350f8640655d2270469332c6e903c2
Ahmad Saud Manzar <ahmad.manzar@queensu.ca>
2020-05-28 08:26:49 -0400

    ejs templates added

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require('path');
 const app = express(); 
 
 // Setting templating engine
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 // Router Object 
@@ -19,13 +19,14 @@ const shopRoutes = require('./routes/shop')
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public'))); 
 
-app.use('/admin',adminRoutes); 
+app.use('/admin',adminRoutes.routes); 
 app.use(shopRoutes); 
 
 // 404 Error Route, when no matches
 app.use((req, res, next) => {
     //res.status(404).send('<h1>Page Not Found</h1>')
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle : 'Page Not Found'}); 
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,26 @@
-const express = require('express'); 
+const express = require('express');
 
-const path = require('path'); 
+const path = require('path');
 
 const rootDir = require('../util/path')
 
-const router = express.Router(); 
+const router = express.Router();
 
-router.get('/', (req, res, next) =>{
+const adminData = require('./admin')
+
+router.get('/', (req, res, next) => {
     //console.log('In the next Middleware'); 
     //res.send('<h1>Hello From Express</h1>')
     //res.sendFile(path.join(__dirname,'../', 'views', 'shop.html')); 
-    res.sendFile(path.join(rootDir, 'views', 'shop.html')); 
+    //res.sendFile(path.join(rootDir, 'views', 'shop.html')); 
+    res.render('shop', {
+        prods: adminData.products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: adminData.products.length > 0,
+        activeShop: true,
+        productsCSS: true
+    });
 });
 
 module.exports = router; 
