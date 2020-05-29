fb81e83304ebc218f9c0e90ab4b80930528b81af
Ahmad Saud Manzar <ahmad.manzar@queensu.ca>
2020-05-29 09:04:19 -0400

    registered routes, simple data model

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const path = require('path');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 // pass controller reference 
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product',productsController.postAddProduct);
+// /admin/products => GET 
+router.get('/products', adminController.getProducts);
+
+router.post('/add-product',adminController.postAddProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,15 @@ const router = express.Router();
 
 const adminData = require('./admin')
 
-const productsController = require('../controllers/products.js');
+const shopController = require('../controllers/shop');
 
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/checkout', shopController.getCheckout);
+
 
 module.exports = router; 
