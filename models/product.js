73e12cbe3405545a516c3ddad61f8ff7cdcb7d5f
Ahmad Saud Manzar <ahmad.manzar@queensu.ca>
2020-06-07 19:20:51 -0400

    add to cart functional with file persistance

--- models/product.js
+++ models/product.js
@@ -2,25 +2,27 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(path.dirname(process.mainModule.filename),
-'data',
-'products.json');
+    'data',
+    'products.json'
+);
 
 const getProductsFromFile = (cb) => {
     fs.readFile(p, (err, fileContent) => {
         if (err) return cb([]);
         else cb(JSON.parse(fileContent));
     })
-}
+};
 
 module.exports = class Product {
     constructor(title, imageUrl, description, price) {
         this.title = title;
-        this.imageUrl = imageUrl; 
-        this.description = description; 
-        this.price = price; 
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             // make sure to use arrow function here 
             products.push(this);
@@ -32,5 +34,12 @@ module.exports = class Product {
 
     static fetchAll(cb) {
         getProductsFromFile(cb);
+    }
+
+    static findById(id, cb) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            cb(product);
+        });
     }
 }
